a6dbeb36bd6d8563dccb53500bbaccd9ffa5c84f
Marcus Eriksson <marcus.eriksson00@hotmail.com>
2020-11-25 14:28:51 +0100

    Employee menu improved

--- app/UI/admin_menu.py
+++ app/UI/admin_menu.py
@@ -1,7 +1,4 @@
-from UI.store_menu import store_menu
-from UI.car_model_menu import car_model_menu
 from UI.employee_menu import employees_menu
-from UI.product_menu import products_menu
 
 
 def admin_menu():
@@ -11,8 +8,6 @@ def admin_menu():
         print("1. Butiker")
         print("2. Anställda")
         print("3. Ordrar")
-        print("4. Produkter")
-        print("5. Bilmodeller")
         print("10. Huvudmeny")
 
         selected = input("> ")
@@ -22,10 +17,6 @@ def admin_menu():
             employees_menu()
         elif selected == "3":
             pass
-        elif selected == "4":
-            products_menu()
-        elif selected == "5":
-            car_model_menu()
         elif selected == "10":
             break
         else:
